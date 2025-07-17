92dcb6410cd84f8c787100f9df3b76eddd03e0a0
Andrew O'Mahony <andrew.omahony@saronic.com>
2025-07-17 13:03:59 -0500

    Modbus compilation

--- flake.nix
+++ flake.nix
@@ -18,11 +18,28 @@
         pkgs = import nixpkgs { inherit system; };
       in
       {
-        packages.default = pkgs.stdenv.mkDerivation {
+        packages.default = pkgs.stdenv.mkDerivation rec {
           pname = "revpi-modbus";
           version = "1.0.0";
           
           src = ./.;
+            
+          # !!! WHY DOESN'T NIX COPY SUBMODULE CODE IN BY DEFAULT?!?! 
+          # !!! We have to fetch it ourselves and do a postUnpack step,
+          # !!! which is really annoying
+          piControlSrc = pkgs.fetchFromGitHub {
+            owner = "RevolutionPi";
+            repo = "piControl";
+            # The source code demands this specific revision, as it has the piTest code in it as
+            # well to read/write from the piControl kernel module.  Gross.
+            rev = "2324c1f8ebfae7cc8e74ba389269d8ff0d9d3066";
+            sha256 = "sha256-UODRNcAFp3TeQkWA0WMI74SwayaID3bxNpRm1fo42zw=";
+          };
+          
+          postUnpack = ''
+            cp -r ${piControlSrc} $sourceRoot/piControl
+            chmod -R u+w $sourceRoot/piControl
+          '';
           
           nativeBuildInputs = with pkgs; [
             cmake
